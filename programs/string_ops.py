d6c10b56df9af73d60244c3bca135f4d301b5e73
dhanraju <dhanunjay.raju@gmail.com>
2016-07-13 18:04:11 -0700

    draft code commit of strings_ops.py

---
+++ programs/string_ops.py
@@ -0,0 +1,12 @@
+"""This module performs operations on strings."""
+
+def string_count(string):
+    """Calculates number of characters present in a string."""
+
+
+def main():
+    """Start of program."""
+
+
+if __name__ == '__main__':
+    main()
