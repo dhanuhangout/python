ec7497f4a257bf300574c9a0339d8525af600350
dhanuhangout <hangouttest10@gmail.com>
2016-07-14 17:05:14 -0700

    committing strings.py

---
+++ practise/strings.py
@@ -0,0 +1,17 @@
+print
+print "***** Given Strings ****"
+str1 = "          3333333this is STR1 string333333          "
+str2 = "string2"
+print "str1 = '%s'" % str1
+print "str2 = '%s'" % str2
+
+print
+print "***** String Methods *****"
+print "str1.lower() = '%s'" % str1.lower()
+print "str1.upper() = '%s'" % str1.upper()
+print "str1.lstrip() = '%s'" % str1.lstrip()
+print "str1.rstrip() = '%s'" % str1.rstrip()
+print "str1.strip(\" 3\") = '%s'" % str1.strip(" 3")
+print "str1.swapcase() = '%s'" % str1.swapcase()
+print "\",\".join(str2) = '%s'" % ",".join(str2)
+print
