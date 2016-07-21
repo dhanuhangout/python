fb34254289ccf9390de176fe0784f3b2d562a28b
dhanuhangout <hangouttest10@gmail.com>
2016-07-21 11:59:36 -0700

    Arithmetic ops using dictionary with lambda forms.

--- problems/programs/arith_ops.py
+++ problems/programs/arith_ops.py
@@ -1,4 +1,8 @@
-"""This module performs arithmetic operations."""
+"""This module performs arithmetic operations.
+Performing arithmetic operations demonstrates in two ways.
+1. Functions as dictionary values.
+2. Lambda forms as dictionary values.
+"""
 
 def add_two_nums(num1, num2):
     """Performs addition of two numbers."""
@@ -29,6 +33,13 @@ DICT_OBJ = {
     5 : mod_two_nums
 }
 
+DICT_OBJ_LAMBDA = {
+    1 : lambda num1, num2: num1+num2,
+    2 : lambda num1, num2: num1-num2,
+    3 : lambda num1, num2: num1*num2,
+    4 : lambda num1, num2: num1/num2,
+    5 : lambda num1, num2: num1%num2
+}
 
 def main():
     """Starting of the program."""
@@ -36,11 +47,19 @@ def main():
     print "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division"
     print "5. Modulo"
     option = int(raw_input())
+
+    print "Enter two numbers:"
+    num1 = int(raw_input())
+    num2 = int(raw_input())
     if option in DICT_OBJ.keys():
-        print "Enter two numbers:"
-        num1 = int(raw_input())
-        num2 = int(raw_input())
         print DICT_OBJ[option](num1, num2)
+        print "Solution using lamdba form: ", DICT_OBJ_LAMBDA[option](num1,
+                num2)
+
+    # Lambda form demonstration.
+    # print "Solution using lambda form"
+        # for key_val in DICT_OBJ_LAMBDA:
+        # print DICT_OBJ_LAMBDA[key_val](num1, num2)
 
 
 if __name__ == '__main__':
