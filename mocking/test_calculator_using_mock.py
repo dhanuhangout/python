511d61914c03e177397926f629f75a50b47c50ad
dhanraju <dhanunjay.raju@gmail.com>
2017-08-31 17:14:49 -0700

    Mocking referenced added.

--- mocking/test_calculator_using_mock.py
+++ mocking/test_calculator_using_mock.py
@@ -21,3 +21,13 @@ class TestCalculator(TestCase):
 
 # How to run this file:
 # py.test <file_name.py>
+
+
+# Other Refs:
+# https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock
+# https://docs.python.org/3/library/unittest.mock-examples.html
+# https://myadventuresincoding.wordpress.com/2011/02/26/python-python-mock-cheat-sheet/
+# http://www.alexandrejoseph.com/blog/2015-08-21-python-mock-example.html
+# https://www.programcreek.com/python/example/15532/mock.side_effect
+# https://blog.fugue.co/2016-02-11-python-mocking-101.html
+
