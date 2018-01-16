5cdd917581b957c1e35ac772b14b3ac2928435dc
dhanraju <dhanunjay.raju@gmail.com>
2018-01-16 00:13:16 -0800

    new test added.

--- maths/number_system_test.py
+++ maths/number_system_test.py
@@ -5,10 +5,15 @@ class TestNumberSystemConversion(unittest.TestCase):
     def setUp(self):
     	self.conversion = number_system.Conversion()
 
-    def test_anybase_to_decimal(self):
+    def test_anybase_to_decimal_01(self):
         ret_value = self.conversion.anybase_to_decimal('101101', 2)
         print 'ret_value = ', ret_value
         assert(ret_value == 45)
+
+    def test_anybase_to_decimal_02(self):
+        ret_value = self.conversion.anybase_to_decimal('565', 8)
+        print 'ret_value = ', ret_value
+        assert(ret_value == 373)
 
 
 if __name__ == '__main__':
