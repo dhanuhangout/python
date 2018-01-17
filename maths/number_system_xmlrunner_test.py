1161786bdfbcc2bcf390cfe6ea0e0fef8d2b52ea
dhanraju <dhanunjay.raju@gmail.com>
2018-01-16 22:17:36 -0800

    xmlrunner support for maths tests.

---
+++ maths/number_system_xmlrunner_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import number_system
+import unittest
+import xmlrunner
+
+class TestNumberSystemConversion(unittest.TestCase):
+    def setUp(self):
+    	self.conversion = number_system.Conversion()
+
+    def test_anybase_to_decimal_01(self):
+        ret_value = self.conversion.anybase_to_decimal('101101', 2)
+        print 'ret_value = ', ret_value
+        assert(ret_value == 45)
+
+    def test_anybase_to_decimal_02(self):
+        ret_value = self.conversion.anybase_to_decimal('565', 8)
+        print 'ret_value = ', ret_value
+        assert(ret_value == 373)
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
---
+++ run_xmlrunner_tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e 
+
+# . ~/.virtualenvs/python2.7/bin/activate
+
+./maths/number_system_xmlrunner_test.py
