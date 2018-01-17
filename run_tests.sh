a741329eb339011285105c220219f968a4a5c3f6
dhanraju <dhanunjay.raju@gmail.com>
2018-01-16 22:43:11 -0800

    flag added to build file.

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ set -e
 # . ~/.virtualenvs/python2.7/bin/activate
 
 bazel test bazel/example01/main:helloworld_test
-bazel test maths:number_system_conversion_test
+bazel test maths:number_system_conversion_test --test_verbose_timeout_warnings
