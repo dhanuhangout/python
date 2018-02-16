7ff27978da7765d054bf23c4b0c266920c2e9a4c
dhanraju <dhanunjay.raju@gmail.com>
2018-02-16 13:34:54 -0800

    Pylint added to coverage with TODO note.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -9,6 +9,8 @@ PYTHONPATH=. python -m coverage run number_system_xmlrunner.py
 python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
 
-# TODO: Add pylint.
+# TODO(dhanraju): Check the given directory depth covers all .py files.
+pylint --rcfile=pylint.cfg $(find . -maxdepth 2 -name "*.py" -print) main/ > pylint.log || exit 0
+
 # pep8 --max-line-length=120 pystache > pep8.log || true
 # pyflakes pystache > pyflakes.log || true
