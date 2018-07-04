d77eb69f9baa141783dc0c13f445fb207253a0ff
dhanraju <dhanunjay.raju@gmail.com>
2018-07-03 17:02:02 -0700

    More parametrized examples.

---
+++ unit_testing/parametrized_testing/test_parametrized_testid.py
@@ -0,0 +1,16 @@
+import pytest
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('3+5', 8),
+    pytest.param('1+7', 8,
+                  marks=pytest.mark.basic),
+    pytest.param('2+4', 6,
+                  marks=pytest.mark.basic,
+                  id='basic_2+4'),
+    pytest.param('6*9', 42,
+                  marks=[pytest.mark.basic, pytest.mark.xfail],
+                  id='basic_6*9'),
+])
+
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
